90dd39bccf2ba5e756b152edddea4eafea9fb931
eharvey328 <emh90@zips.uakron.edu>
2017-04-27 02:40:12 -0400

    Get basic version of A* running

--- AStar.js
+++ AStar.js
@@ -1,31 +1,173 @@
-var cols = 5;
-var rows = 5;
-var grid = new Array()
+var cols = 10;
+var rows = 10;
+var grid = new Array(cols);
 
-function Spot() {
-	this.f = 0;
-	this.g = 0;
-	this.h = 0;
+var openSet = []; // all nodes to be checked
+var closedSet = []; // all nodes checked
+var start;
+var end;
+var w, h;
+var path = [];
+
+function Spot(i, j) {
+	// Spot position in grid
+	this.i = i;
+	this.j = j;
+
+	this.f = 0; // function value of g and h
+	this.g = 0; // "g score" (total cost at pos)
+	this.h = 0; // heuristic (heading the right dir)
+	this.neighbors = []; // all spots touching this spot
+	this.previous = undefined;
+
+	// display the spot on the grid
+	this.show = function(color) {
+		fill(color);
+		noStroke();
+		rect(this.i * w, this.j * h, w-1, h-1); // size is based on the grid
+	}
+
+	this.addNeighbors = function(grid) {
+		if (this.i < cols - 1) {
+			this.neighbors.push(grid[this.i+1][this.j]);
+		}
+		if (this.i > 0) {
+			this.neighbors.push(grid[this.i-1][this.j]);
+		}
+		if (this.j < rows-1) {
+			this.neighbors.push(grid[this.i][this.j+1]);
+		}
+		if (this.j > 0) {
+			this.neighbors.push(grid[this.i][this.j-1]);
+		}
+	}
 }
 
 function setup() {
 	createCanvas(400,400);
-	console.log('A*');
+
+	// calculate the number of spots horiz and vert
+	w = width / cols;
+	h = height / rows;
 
 	//create 2d array
-	for(var i = 0; i < cols; i++) {
+	for (var i = 0; i < cols; i++) {
 		grid[i] = new Array(rows);
 	}
 
-	for(var i = 0; i < cols; i++) {
+	for (var i = 0; i < cols; i++) {
 		for(var j = 0; j < rows; j++) {
-			grid[i][j] = new Spot();
+			grid[i][j] = new Spot(i, j);
 		}
 	}
+
+	for (var i = 0; i < cols; i++) {
+		for(var j = 0; j < rows; j++) {
+			grid[i][j].addNeighbors(grid);
+		}
+	}
+
+	//find path from top left to bot right
+	start = grid[0][0];
+	end = grid[cols-1][rows-1];
+
+	openSet.push(start);
+
 
 	console.log(grid);
 }
 
+// this function is looping continuously on the page
 function draw() {
+
+	if (openSet.length > 0) {
+		var lowestPos = 0;
+		for (var i = 0; i < openSet.length; i++) {
+			if (openSet[i].f < openSet[lowestPos].f) {
+				lowestPos = i;
+			}
+		}
+
+		var current = openSet[lowestPos];
+		if (current == end) {
+			noLoop();
+			console.log("Finished");
+		}
+
+		removeFromArray(openSet, current);
+		closedSet.push(current);
+
+		var neighbors = current.neighbors;
+		for (var i = 0; i < neighbors.length; i++) {
+			var neighbor = neighbors[i];
+
+			if (!closedSet.includes(neighbor)) {
+				var tempG = current.g + 1;
+
+				if (openSet.includes(neighbor)) {
+					// already in openSet
+					// update the value if lower
+					if(tempG < neighbor.g) {
+						neighbor.g = tempG;
+					}
+				} else {
+					// add to openSet
+					neighbor.g = tempG;
+					openSet.push(neighbor);
+				}
+
+				neighbor.h = heuristic(neighbor, end);
+				neighbor.f = neighbor.g + neighbor.h;
+				neighbor.previous = current;
+
+
+			}	
+		}
+	} else {
+		// no solution
+	}
+
 	background(0);
+
+	for (var i = 0; i < cols; i++) {
+		for(var j = 0; j < rows; j++) {
+			grid[i][j].show(color(255));
+		}
+	}
+
+	for (var i = 0; i < closedSet.length; i++) {
+		closedSet[i].show(color(255,0,0));
+	}	
+
+	for (var i = 0; i < openSet.length; i++) {
+		openSet[i].show(color(0,255,0));
+	}
+
+	// find the path
+	path = [];
+	var temp = current;
+	path.push(temp);
+	while (temp.previous) {
+		path.push(temp.previous);
+		temp = temp.previous;
+	}
+
+	for (var i = 0; i < path.length; i++) {
+		path[i].show(color(0,0,255));
+	}
+}
+
+// this can be optimized later
+function removeFromArray(arr, element) {
+	for (var i = arr.length-1; i>=0; i--) {
+		if (arr[i] == element) {
+			arr.splice(i, 1);
+		}
+	}
+}
+
+// checks how far away a point is from another using manhattan dist formula
+function heuristic(a, b) {
+	var d = abs(a.i - b.i) + abs(a.j - b.j);
+	return d;
 }
